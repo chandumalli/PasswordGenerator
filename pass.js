e5b69d68c69a21154457de2f0bff3230b39fff04
CHANDU MALLI <132278347+chandumalli@users.noreply.github.com>
2024-04-16 19:25:04 +0530

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,16 @@
+document.getElementById('generate').addEventListener('click', function() {
+  var length = document.getElementById('length').value;
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  document.getElementById('password').value = password;
+});
+
+document.getElementById('copy').addEventListener('click', function() {
+  var password = document.getElementById('password');
+  password.select();
+  document.execCommand('copy');
+  alert('Password copied to clipboard!');
+});
